5d5d64024982b9381a9d6bf8d5e1aa886fa85186
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-04-12 21:16:35 +0530

    .....

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -1,10 +1,33 @@
+
+import { useState } from "react";
 import CustomButton from "./CustomButton";
 import styles from "../../styles/pages/home.module.scss";
 import HeaderLogo from "../img/home_logo.png";
 import HomeBanner from "../img/home_banner.png";
 import DownArrow from "../img/down_arrow.png";
+import KingLogo from "../img/king_logo.svg";
+import CentrePhoto from "../img/centre_photo.png";
+import { useForm } from "react-hook-form";
+
 
 export default function HomePage({ onNext }: { onNext: () => void }) {
+  const [userName, setUserName] = useState("");
+
+	const {
+		register,
+		handleSubmit,
+		reset,
+		watch,
+		formState: { errors },
+	} = useForm();
+
+
+const onSubmit = (data: any) => {
+  setUserName(data.name);
+  console.log("User Name stored in state:", data.name);
+  onNext();
+};
+console.log(userName, "userName");
 
     
     return (
@@ -16,6 +39,14 @@ export default function HomePage({ onNext }: { onNext: () => void }) {
 
           <div className={styles.home_banner}>
             <img src={HomeBanner.src} alt="HomeBanner" />
+            <div className={styles.logo_banner}>
+              <div className={styles.home_logo}>
+                <img src={KingLogo.src} alt="HomeBanner" />
+              </div>
+              <div className={styles.home_center}>
+                <img src={CentrePhoto.src} alt="HomeBanner" />
+              </div>
+            </div>
           </div>
 
           <div className={styles.banner_title}>
@@ -23,51 +54,61 @@ export default function HomePage({ onNext }: { onNext: () => void }) {
           </div>
       
           <img src={DownArrow.src} className={styles.down_arrow} alt="DownArrow" />
-          {/* <img src={DownArrow.src} className={styles.down_arrow} alt="DownArrow" /> */}
 
           <div className={styles.home_avatar_section}>
             <h2 className={styles.home_avatar_1}>Unleash your <br/> avatar, K-STYLE!</h2>
             <h2 className={styles.home_avatar_2}>Unleash your <br/> avatar, K-STYLE!</h2>
           </div>
 
-          <form className={`${styles.form}`}>
-				<div className={styles.formGroupSection}>
-					<div className={styles.formGroup}>
-						<label className={`${styles.lable_text}`}>
-              Enter name to begin<span>*</span>
-						</label>
-						<input
-							className={`${styles.input} text_xs`}
-							type="text"
-							id="name"
-							name="name"
-							placeholder="Enter name to begin*"
-						/>
-					</div>
-				</div>
-				<div className={styles.formGroupSection}>
-					<div className={`${styles.formGroup} ${styles.check_box}`}>
-						<input
-							type="checkbox"
-							id="terms"
-							name="terms"
-						/>
-						
-						<label className={styles.agree} htmlFor="terms">
-						I agree to receive updates from Burger King
-							{/* <a
-								href="https://www.lubrizol.com/Legal/Lubrizol-Privacy-and-Cookie-Policy"
-								target="_blank"
-							>
-								terms of use
-							</a> */}
-						</label>
-					</div>
-				</div>
-				<div className={`${styles.btn_section}`}>
-          <CustomButton id="nextBtn" text="Try now" onClick={onNext} />
-				</div>
-			</form>
+          <form onSubmit={handleSubmit(onSubmit)} className={`${styles.form}`}>
+  <div className={styles.formGroupSection}>
+    <div className={styles.formGroup}>
+      <label className={`${styles.lable_text}`}>
+        Enter name<span>*</span>
+      </label>
+      <input
+        className={`${styles.input} text_xs`}
+        type="text"
+        id="name"
+        placeholder="Enter name"
+        {...register("name", { required: true })}
+      />
+      {errors.name && errors.name.type === "required" && (
+        <label className={styles.error}>This field is required</label>
+      )}
+    </div>
+  </div>
+
+  <div className={styles.formGroupSection}>
+    <div className={`${styles.formGroup} ${styles.check_box}`}>
+      <input
+        type="checkbox"
+        id="terms"
+        {...register("terms", { required: "This field is required" })}
+      />
+      {errors.terms && (
+        <label className={styles.error}>This field is required</label>
+      )}
+
+      <label className={styles.agree} htmlFor="terms">
+        I agree to receive updates from Burger King
+      </label>
+    </div>
+  </div>
+
+  <div className={`${styles.btn_section_next}`}>
+      {userName ? (
+      <button id="nextBtn" onClick={onNext} type="submit" className={styles.next}>
+        Start Now
+      </button>
+    ) : (
+      <button type="submit" className={styles.next}>
+        Start Now
+      </button>
+    )}
+  </div>
+</form>
+
         </div>
 
      
